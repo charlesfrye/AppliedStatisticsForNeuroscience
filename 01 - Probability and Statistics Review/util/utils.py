13323ce2d8ed02c4420f0a658a3832f1ad02d970
Charles Frye <charlesfrye@berkeley.edu>
2017-07-26 11:12:21 -0700

    #28 and #29 for 01

--- 01 - Probability and Statistics Review/util/utils.py
+++ 01 - Probability and Statistics Review/util/utils.py
@@ -3,39 +3,40 @@ import scipy.integrate as integrate
 from IPython.core.display import HTML
 import numpy as np
 
-def formatDataframes():
+def format_dataframes():
     css = open('./css/style-table.css').read()
     return HTML('<style>{}</style>'.format(css))
 
-def plotPMF(PMF,title=''):
+def plot_PMF(PMF,title=''):
     plt.figure()
     if PMF == []:
         return
     assert all([p >= 0 for p in PMF]), "not a valid PMF: negative numbers"
     assert abs(1-sum(PMF))<=10**-5, "not a valid PMF: doesn't sum to 1"
     linewidth = 4
-    numElements = len(PMF)
+    num_elements = len(PMF)
     plt.plot(PMF,marker='o',linestyle='none',
          markerfacecolor='white',markeredgecolor='black',
          markeredgewidth=linewidth,markersize=12)
-    plt.vlines(range(numElements),[0]*numElements,PMF,
+    plt.vlines(range(num_elements),[0]*num_elements,PMF,
          linewidth=linewidth)
-    heightenYLim(plt.gca())
+    heighten_YLim(plt.gca())
     plt.title(title,fontsize=24)
 
-def plotPDF(PDF,title=''):
+def plot_PDF(PDF,title=''):
     plt.figure()
     eps = 10**-3; edge=10
     xs = np.arange(-edge*eps,1+edge*eps,eps)
     plt.plot(xs,[PDF(x) for x in xs],
             marker='None',linewidth=4)
-    heightenYLim(plt.gca())
+    heighten_YLim(plt.gca())
+    plt.title(title,fontsize=24)
 
-def heightenYLim(ax):
+def heighten_YLim(ax):
     ylim = ax.get_ylim()
     ylim = (ylim[0],ylim[1]*1.1)
     ax.set_ylim(ylim)
 
-def integratesToOne(PDF):
-    integral, errBound = integrate.quad(PDF,0,1)
-    return abs(1-integral) <= errBound
+def integrates_to_one(PDF):
+    integral, err_bound = integrate.quad(PDF,0,1)
+    return abs(1-integral) <= err_bound
