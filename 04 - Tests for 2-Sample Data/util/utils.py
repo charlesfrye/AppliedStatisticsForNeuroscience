f2c56fad6c3167ba25eebab19a4b01015a4a9d83
Charles Frye <charlesfrye@berkeley.edu>
2017-08-03 18:26:58 -0700

    Completes draft of tutorial and adjusts utils

--- 04 - Tests for 2-Sample Data/util/utils.py
+++ 04 - Tests for 2-Sample Data/util/utils.py
@@ -10,28 +10,44 @@ def formatDataframes():
 
 def differencePlot(tidyFrame,ylabel="outcome"):
     """
-    Accepts as first arg a tidy-format dataFrame with 
-    exactly two numeric-valued columns and plots it as a 
-    "difference plot" -- side-by-side stripplots for each 
-    numeric column, with observations from the same row 
+    Accepts as first arg a tidy-format dataFrame with
+    exactly two numeric-valued columns and plots it as a
+    "difference plot" -- side-by-side stripplots for each
+    numeric column, with observations from the same row
     joined by a line.
-    
+
     Optionally also accepts a label for the y-axis as kwarg ylabel.
     """
-    
+
     sns.stripplot(data=tidyFrame,
                   size=16,linewidth=4,
                   edgecolor="k",color='lightcoral');
-    
+
     # from StackOverflow 25039626 -- select only numeric columns
     numerics = tidyFrame.select_dtypes(include=[np.number])
-    
+
     assert numerics.shape[1] == 2, "dataFrame needs exactly two numeric columns!"
-    
+
     xs = np.asarray([[0,1]]*(numerics.shape[1])).T
     ys = np.asarray(numerics).T
-    
+
     plt.plot(xs,ys,
              linewidth=4,color='k');
-    
+
     plt.ylabel(ylabel)
+
+def make_shuffle(tidy_dataframe,):
+    """
+    Makes and returns a shuffled copy of a tidy dataframe with two columns.
+    Here, "shuffling" means swapping the column indices for a random subset
+    of rows, as is needed for performing a randomization test
+    on a test statistic computed from column differences in the original dataframe.
+    """
+
+    shuffle = tidy.copy()
+
+    for index in range(tidy.shape[0]):
+        if np.random.choice([True, False]):
+            shuffle.iloc[index,:] = tidy.iloc[index,::-1].as_matrix()
+
+    return shuffle
