0c4d2ff620521bd7474ccaa5ab55ace3c2851ab0
Charles Frye <charlesfrye@berkeley.edu>
2017-08-09 15:15:57 -0700

    #29 for utils of ANOVA II

--- 06 - ANOVA II/util/utils.py
+++ 06 - ANOVA II/util/utils.py
@@ -6,24 +6,24 @@ import string
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
 
-def generateData(numSubjects,numFactors,standardDev):
+def generate_data(num_subjects,num_factors,standard_dev):
 
-    outcome = np.random.normal(scale=standardDev,size=(1,numSubjects))
-    predictors = np.random.binomial(1,0.5,size=(numFactors,numSubjects))
+    outcome = np.random.normal(scale=standard_dev,size=(1,num_subjects))
+    predictors = np.random.binomial(1,0.5,size=(num_factors,num_subjects))
     data = np.vstack([outcome,predictors])
-    
+
     df = {}
-    
+
     labels = ['outcome']+list(string.ascii_lowercase)
-    
-    for factorData,factorLabel in zip(data,labels):
-        df[factorLabel] = factorData
-        
+
+    for factor_data,factor_label in zip(data,labels):
+        df[factor_label] = factor_data
+
     df = pd.DataFrame.from_dict(df)
-    
+
     return df
 
-def runANOVA(df):
+def run_ANOVA(df):
     predictors = '*'.join(df.columns[:-1])
     ols_lm = smf.ols('outcome ~ '+predictors,data=df)
     fit = ols_lm.fit()
