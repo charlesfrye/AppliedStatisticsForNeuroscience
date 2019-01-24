1a552ffc7274b3414b25ab8246d76ad5ca037467
Charles Frye <charlesfrye@berkeley.edu>
2019-01-24 13:35:53 -0800

    updates shared.py files with fig_scale control

--- Part 02 - Statistical Modeling/09 - Model Specification/util/shared.py
+++ Part 02 - Statistical Modeling/09 - Model Specification/util/shared.py
@@ -5,11 +5,11 @@ def format_dataframes():
     css = open("./css/style-table.css").read()
     return HTML("<style>{}</style>".format(css))
 
-def format_plots(colorblind=False):
+def format_plots(fig_scale=10., colorblind=False):
     if colorblind:
         palette = "colorblind"
     else:
         palette = "deep"
     sns.set(color_codes=True, palette=palette,
-        font_scale=2, context="notebook",
-	rc = {'figure.figsize':[9.6,7.5]})
+        font_scale=1.5, context="notebook",
+        rc = {'figure.figsize':[fig_scale, fig_scale]})
